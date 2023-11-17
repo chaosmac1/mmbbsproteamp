d69a65e600fecf722440df803fb380a0b9d47bdc
chaosmac1 <lambda@cyberpit.de>
2023-11-17 22:46:27 +0100

    feat(SQL): Add SQL File

---
+++ Backend/SensorApi/Sensor.Db/Postgresql.sql
@@ -0,0 +1,30 @@
+CREATE TABLE IF NOT EXISTS user_info
+(
+    id            uuid    NOT NULL PRIMARY KEY,
+    username      TEXT    NOT NULL UNIQUE,
+    password_hash TEXT    NOT NULL,
+    is_admin      boolean NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user_cookie
+(
+    id           TEXT      NOT NULL PRIMARY KEY,
+    user_id      uuid      NOT NULL REFERENCES user_info (id) ON DELETE CASCADE,
+    end_datetime timestamp NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS iot_device
+(
+    id              uuid      NOT NULL PRIMARY KEY,
+    name            TEXT      NOT NULL UNIQUE,
+    last_request    timestamp NOT NULL,
+    allowed_request boolean   NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS graph
+(
+    id           BIGSERIAL NOT NULL PRIMARY KEY,
+    device_id    UUID      NOT NULL REFERENCES iot_device (id) ON DELETE CASCADE,
+    time         timestamp,
+    value_kelvin FLOAT
+);
