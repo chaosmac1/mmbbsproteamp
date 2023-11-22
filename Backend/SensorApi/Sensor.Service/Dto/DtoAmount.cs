87e1bad86c149c3c26af9d10a84436b21bb95968
chaosmac1 <lambda@cyberpit.de>
2023-11-22 22:42:30 +0100

    fix(Dto): Impl IDto, IDtoFrom<DtoAmount, IAmount> for DtoAmount

--- Backend/SensorApi/Sensor.Service/Dto/DtoAmount.cs
+++ Backend/SensorApi/Sensor.Service/Dto/DtoAmount.cs
@@ -1,5 +1,10 @@
+using Sensor.Service.AttachmentService.Interface;
+using Sensor.Service.Port.Interface;
+
 namespace Sensor.Service.Dto; 
 
-public record DtoAmount(int Amount) {
-    
+public record DtoAmount(int Amount) : IDto, IDtoFrom<DtoAmount, IAmount> {
+    public static DtoAmount From(IAmount from) {
+        return new DtoAmount(from.Amount);
+    }
 }
